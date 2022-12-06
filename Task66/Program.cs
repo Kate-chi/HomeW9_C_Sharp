62257934af7164785a03e6a60758eba6d17ec469
Kate <c.chistova2014@yandex.ru>
2022-12-06 20:23:20 +0300

    сумма от M до N

---
+++ Task66/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 66: Задайте значения M и N. 
+Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+Выполнить с помощью рекурсии.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.Write("Введите число A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(FindSumNaturalNumbers(numA, numB));
+
+int FindSumNaturalNumbers(int a, int b)
+{
+    if(a == b)
+    {
+        return b;
+    }
+    else if(a < b)
+    {
+        return a + FindSumNaturalNumbers(a + 1, b);
+    }
+    else
+    {
+        return a + FindSumNaturalNumbers(a - 1, b);
+    }
+}
